a9a82b6418d82e8ffbb2493047cca7b3234b7599
Victor Olvera <vhocar@gmail.com>
2023-02-21 09:54:42 -0700

    test

--- dist/esm/components/elements/TextInput.js
+++ dist/esm/components/elements/TextInput.js
@@ -29,7 +29,7 @@ var MyInput = styled(OutlinedInput)({
     }
 });
 export var TextInput = function (_a) {
-    var autoComplete = _a.autoComplete, autoFocus = _a.autoFocus, classes = _a.classes, defaultValue = _a.defaultValue, _b = _a.disabled, disabled = _b === void 0 ? false : _b, error = _a.error, id = _a.id, icon = _a.icon, name = _a.name, onChange = _a.onChange, placeholder = _a.placeholder, _c = _a.iconPosition, iconPosition = _c === void 0 ? 'start' : _c, _d = _a.fullWidth, fullWidth = _d === void 0 ? false : _d, _e = _a.multiline, multiline = _e === void 0 ? false : _e, _f = _a.readOnly, readOnly = _f === void 0 ? false : _f, _g = _a.required, required = _g === void 0 ? false : _g, _h = _a.rows, rows = _h === void 0 ? 1 : _h, sx = _a.sx, type = _a.type, value = _a.value, endIconOnClick = _a.endIconOnClick, props = __rest(_a, ["autoComplete", "autoFocus", "classes", "defaultValue", "disabled", "error", "id", "icon", "name", "onChange", "placeholder", "iconPosition", "fullWidth", "multiline", "readOnly", "required", "rows", "sx", "type", "value", "endIconOnClick"]);
+    var autoComplete = _a.autoComplete, autoFocus = _a.autoFocus, classes = _a.classes, defaultValue = _a.defaultValue, _b = _a.disabled, disabled = _b === void 0 ? false : _b, error = _a.error, id = _a.id, icon = _a.icon, name = _a.name, onChange = _a.onChange, placeholder = _a.placeholder, _c = _a.iconPosition, iconPosition = _c === void 0 ? 'start' : _c, _d = _a.fullWidth, fullWidth = _d === void 0 ? false : _d, _e = _a.multiline, multiline = _e === void 0 ? false : _e, _f = _a.readOnly, readOnly = _f === void 0 ? false : _f, _g = _a.required, required = _g === void 0 ? false : _g, _h = _a.rows, rows = _h === void 0 ? 1 : _h, sx = _a.sx, type = _a.type, value = _a.value, endIconOnClick = _a.endIconOnClick, iconStyle = _a.iconStyle, props = __rest(_a, ["autoComplete", "autoFocus", "classes", "defaultValue", "disabled", "error", "id", "icon", "name", "onChange", "placeholder", "iconPosition", "fullWidth", "multiline", "readOnly", "required", "rows", "sx", "type", "value", "endIconOnClick", "iconStyle"]);
     return (React.createElement(FormControl, { sx: {
             fontFamily: 'IBM Plex Sans',
             p: 0,
@@ -46,9 +46,7 @@ export var TextInput = function (_a) {
                     React.createElement(FontAwesomeIcon, { icon: icon, style: { color: colors.gray[30] } }))
                 :
                     null, endAdornment: icon && iconPosition === 'end' ?
-                React.createElement(InputAdornment, { position: iconPosition, onClick: endIconOnClick, style: {
-                        cursor: value ? 'pointer' : 'initial'
-                    } },
+                React.createElement(InputAdornment, { position: iconPosition, onClick: endIconOnClick, style: iconStyle },
                     React.createElement(FontAwesomeIcon, { icon: icon, style: {
                             color: value ? colors.gray[70] : colors.gray[30]
                         } }))
