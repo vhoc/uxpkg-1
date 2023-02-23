49ac52230b9d5efd9e840aff71d0322885cdf3c7
Victor Olvera <vhocar@gmail.com>
2023-02-23 09:02:03 -0700

    test

--- dist/cjs/components/elements/SelectTime.js
+++ dist/cjs/components/elements/SelectTime.js
@@ -3,9 +3,7 @@ exports.__esModule = true;
 exports.SelectTime = void 0;
 var tslib_1 = require("tslib");
 var react_1 = tslib_1.__importDefault(require("react"));
-var AdapterMoment_1 = require("@mui/x-date-pickers/AdapterMoment");
 var x_date_pickers_1 = require("@mui/x-date-pickers");
-var x_date_pickers_2 = require("@mui/x-date-pickers");
 //import { Box, TextField } from '@mui/material'
 var material_1 = require("@mui/material");
 var styles_1 = require("@mui/material/styles");
@@ -16,7 +14,7 @@ var SelectTime = function (_a) {
     //const [value, setValue] = useState<any>()
     //const [isOpen, setIsOpen] = useState<boolean>(false)
     var onChange = _a.onChange, closeOnSelect = _a.closeOnSelect, value = _a.value, open = _a.open, _b = _a.openTo, openTo = _b === void 0 ? 'hours' : _b, _c = _a.disabled, disabled = _c === void 0 ? false : _c, getOpenDialogAriaText = _a.getOpenDialogAriaText, inputFormat = _a.inputFormat, inputRef = _a.inputRef, onAccept = _a.onAccept, onClose = _a.onClose, onOpen = _a.onOpen, orientation = _a.orientation, readOnly = _a.readOnly, _d = _a.ampm, ampm = _d === void 0 ? true : _d, _e = _a.views, views = _e === void 0 ? ['hours', 'minutes'] : _e;
-    var MyTimePicker = (0, styles_1.styled)(x_date_pickers_2.TimePicker)({
+    var MyTimePicker = (0, styles_1.styled)(x_date_pickers_1.TimePicker)({
         backgroundColor: theme_1.colors.white,
         fontFamily: 'IBM Plex Sans',
         fontSize: '14px',
@@ -69,9 +67,8 @@ var SelectTime = function (_a) {
             outline: 'none'
         }
     });
-    return (react_1["default"].createElement(x_date_pickers_1.LocalizationProvider, { dateAdapter: AdapterMoment_1.AdapterMoment },
-        react_1["default"].createElement(MyTimePicker, { value: value, closeOnSelect: closeOnSelect, onChange: onChange, open: open, disabled: disabled, getOpenDialogAriaText: getOpenDialogAriaText, inputFormat: inputFormat, inputRef: inputRef, onAccept: onAccept, onClose: onClose, openTo: openTo, onOpen: onOpen, orientation: orientation, readOnly: readOnly, components: { OpenPickerIcon: function () { return react_1["default"].createElement(react_fontawesome_1.FontAwesomeIcon, { size: 'xs', icon: pro_solid_svg_icons_1.faCaretDown }); } }, ampm: ampm, views: views, renderInput: function (params) {
-                return (react_1["default"].createElement(MyInput, tslib_1.__assign({}, params, { size: 'small' })));
-            } })));
+    return (react_1["default"].createElement(MyTimePicker, { value: value, closeOnSelect: closeOnSelect, onChange: onChange, open: open, disabled: disabled, getOpenDialogAriaText: getOpenDialogAriaText, inputFormat: inputFormat, inputRef: inputRef, onAccept: onAccept, onClose: onClose, openTo: openTo, onOpen: onOpen, orientation: orientation, readOnly: readOnly, components: { OpenPickerIcon: function () { return react_1["default"].createElement(react_fontawesome_1.FontAwesomeIcon, { size: 'xs', icon: pro_solid_svg_icons_1.faCaretDown }); } }, ampm: ampm, views: views, renderInput: function (params) {
+            return (react_1["default"].createElement(MyInput, tslib_1.__assign({}, params, { size: 'small' })));
+        } }));
 };
 exports.SelectTime = SelectTime;
