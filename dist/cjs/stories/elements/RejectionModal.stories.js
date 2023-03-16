6a11857ea27b19f20d84ae630957c2e0c2195d5d
Victor Olvera <vhocar@gmail.com>
2023-03-16 10:02:14 -0600

    Implemented RejectionModal component

---
+++ dist/cjs/stories/elements/RejectionModal.stories.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+exports.Default = void 0;
+var RejectionModal_1 = require("../../components/elements/RejectionModal");
+exports["default"] = {
+    title: 'elements/Feedback/RejectionModal',
+    component: RejectionModal_1.RejectionModal
+};
+exports.Default = {
+    args: {
+        title: 'Are you sure you want to reject Request 12312313?',
+        onClickCancel: function () { return alert("Cancelling..."); },
+        onClickApprove: function () { return alert("Rejecting..."); }
+    }
+};
---
+++ dist/esm/components/elements/RejectionModal.js
@@ -0,0 +1,39 @@
+import { __assign } from "tslib";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTriangleExclamation } from '@fortawesome/pro-solid-svg-icons';
+import { colors } from '../../theme';
+import { Typography } from './Typography';
+import { TextInput } from './TextInput';
+import { Button } from './Button';
+export var RejectionModal = function (_a) {
+    var title = _a.title, comments = _a.comments, onCommentsChange = _a.onCommentsChange, onClickCancel = _a.onClickCancel, onClickApprove = _a.onClickApprove, style = _a.style;
+    return (React.createElement("div", { style: __assign({ width: '584px', height: 'auto', paddingTop: '31px', paddingRight: '31px', paddingBottom: '24px', paddingLeft: '31px', display: 'flex', flexDirection: 'column', gap: '28px', backgroundColor: colors.white, borderRadius: '8px', boxShadow: '0px 2px 4px rgba(34, 41, 69, 0.37)' }, style) },
+        React.createElement("div", { style: {
+                display: 'flex',
+                alignItems: 'center',
+                gap: '12px'
+            } },
+            React.createElement(FontAwesomeIcon, { icon: faTriangleExclamation, 
+                //size={'md'}
+                color: colors.yellow[50], style: {
+                    backgroundColor: 'rgba(216, 160, 50, 0.1)',
+                    borderRadius: '100%',
+                    padding: '6px'
+                } }),
+            React.createElement(Typography, { variant: 'h4-medium', style: {
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    width: '100%'
+                } }, title)),
+        React.createElement(TextInput, { label: 'Add comments', multiline: true, rows: 2, onChange: onCommentsChange, value: comments }),
+        React.createElement("div", { style: {
+                display: 'flex',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                gap: '16px'
+            } },
+            React.createElement(Button, { size: "sm", variant: "gray", onClick: onClickCancel }, "Cancel"),
+            React.createElement(Button, { size: "sm", variant: "grayRed", onClick: onClickApprove }, "Reject"))));
+};
